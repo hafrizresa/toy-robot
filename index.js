d85402cec93a0e4a04161a97b19751d239fb7179
hafrizresa <hafrizresa@gmail.com>
2019-03-04 19:51:32 +0700

    left right report feature

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const input = process.argv.slice(2)
 const ToyRobot = require('./simulator/Simulator')
 const Simulator = new ToyRobot
 
-
 switch (input[0]) {
   case "PLACE":
     Simulator.place(input)
     break;
   case "MOVE":
+    Simulator.Move()
     break;
   case "LEFT":
+    Simulator.Left()
     break;
   case "RIGHT":
+    Simulator.Right()
     break;
   case "REPORT":
+    Simulator.Report()
     break;
   default:
-  console.log('PLEASE INPUT CORRECT COMAND')
+    console.log('PLEASE INPUT CORRECT COMAND')
     break;
 }
--- simulator/Simulator.js
+++ simulator/Simulator.js
@@ -2,15 +2,77 @@ const fs = require('fs')
 // let data = JSON.parse(fs.readFileSync('data.json', 'utf8'))
 
 class Simulator {
+
   place(input) {
     let place = input[1].split(',')
     if (this.placeChecker(place)) {
       fs.writeFileSync('data.json', `{"X":${place[0]},"Y":${place[1]}, "F":"${place[2]}"}`, 'utf8')
+    } else {
+      console.log('PLEASE INPUT CORRECT COMAND')
     }
   }
 
-  initiateLocation() {
+  Move() {
+  
+  }
 
+  Right() {
+    let data = fs.readFileSync('data.json', 'utf8')
+    if (data) {
+      let parsed = JSON.parse(data)
+      if (parsed.F) {
+        if (parsed.F === 'NORTH') {
+          this.writeData('', '', 'EAST')
+        } else if (parsed.F === 'EAST') {
+          this.writeData('', '', 'SOUTH')
+        } else if (parsed.F === 'SOUTH') {
+          this.writeData('', '', 'WEST')
+        }
+        else {
+          this.writeData('', '', 'NORTH')
+        }
+
+      } else {
+        console.log("INPUT PLACE FIRST")
+      }
+    } else {
+      console.log('INPUT PLACE FIRST')
+    }
+  }
+
+  Left() {
+    let data = fs.readFileSync('data.json', 'utf8')
+    if (data) {
+      let parsed = JSON.parse(data)
+      if (parsed.F) {
+        if (parsed.F === 'NORTH') {
+          this.writeData('', '', 'WEST')
+        } else if (parsed.F === 'WEST') {
+          this.writeData('', '', 'SOUTH')
+        } else if (parsed.F === 'SOUTH') {
+          this.writeData('', '', 'EAST')
+        }
+        else {
+          this.writeData('', '', 'NORTH')
+        }
+      } else {
+        console.log("INPUT PLACE FIRST")
+      }
+    } else {
+      console.log('INPUT PLACE FIRST')
+    }
+  }
+
+  Report() {
+    let report = fs.readFileSync('data.json', 'utf8')
+    if (report) {
+      let parsed = JSON.parse(report)
+      if (parsed.F) {
+        console.log(parsed.X + ',' + parsed.Y + ',' + parsed.F)
+      }
+    } else {
+      console.log('YOU DONT PLACE ROBOT ANYWHERE')
+    }
   }
 
   placeChecker(input) {
@@ -18,6 +80,16 @@ class Simulator {
       return true
     }
     return false
+  }
+
+  writeData(x, y, f) {
+    let current = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+    let X = x || current.X
+    let Y = y || current.Y
+    let F = f || current.F
+
+    fs.writeFileSync('data.json', `{"X":${X},"Y":${Y}, "F":"${F}"}`, 'utf8')
+
   }
 
 }
